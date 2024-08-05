9d2d26e0e0f5b8e1275e040fac9f2038af1878ca
DesarguesC <425593160@qq.com>
2024-08-05 15:31:46 +0800

    lama&seem

--- process.py
+++ process.py
@@ -13,6 +13,7 @@ from torchvision import transforms
 from PIL import Image
 import rembg
 
+
 class BLIP2():
     def __init__(self, device='cuda'):
         self.device = device
@@ -38,8 +39,32 @@ if __name__ == '__main__':
     parser.add_argument('--model', default='u2net', type=str, help="rembg model, see https://github.com/danielgatis/rembg#models")
     parser.add_argument('--size', default=256, type=int, help="output resolution")
     parser.add_argument('--border_ratio', default=0.2, type=float, help="output border ratio")
-    parser.add_argument('--recenter', type=bool, default=True, help="recenter, potentially not helpful for multiview zero123")    
+    parser.add_argument('--recenter', type=bool, default=True, help="recenter, potentially not helpful for multiview zero123")
+    # SEEM
+    parser.add_argument('--seem_ckpt', type=str, default="../Tools/SEEM/focall_unicl_lang_demo.yaml", help='restore where the SEEM & LaMa model locates')
+    parser.add_argument('--seem_cfg', type=str, default="../Tools/SEEM/seem_focall_v0.pt")
+    # LaMa
+    parser.add_argument('--lama_ckpt', default='../Tools/LaMa/', help='actually path to lama ckpt base folder, ckpt specified in config files')
+    parser.add_argument('--lama_cfg', default='./configs/lama_default.yaml', help='path to lama inpainting config path')
+
+    #Outputs
+    parser.add_argument('--results', default='../v2x-outputs/pre-process/', help='result direction')
+
+
     opt = parser.parse_args()
+
+    """
+        create results directions here ↓
+        MASK: os.path.join(opt.results, 'masks')
+        VIEW: os.path.join(opt.results,  'views')
+    """
+    if not os.path.exists(opt.results):
+        os.mkdir(opt.results)
+    if not os.path.exists(os.path.join(opt.results, 'remove')):
+        os.mkdir(os.path.join(opt.results, 'remove'))
+
+
+    setattr(opt, "device", "cuda" if torch.cuda.is_available() else "cpu")
 
     session = rembg.new_session(model_name=opt.model)
 
@@ -50,6 +75,8 @@ if __name__ == '__main__':
     else: # isfile
         files = [opt.path]
         out_dir = os.path.dirname(opt.path)
+
+
     
     for file in files:
 
@@ -58,9 +85,9 @@ if __name__ == '__main__':
 
         # load image
         print(f'[INFO] loading image {file}...')
-        image = cv2.imread(file, cv2.IMREAD_UNCHANGED)
+        image = cv2.imread(file, cv2.IMREAD_UNCHANGED) # read
         
-        # carve background
+        # TODO: use seem to remove foreground
         print(f'[INFO] background removal...')
         carved_image = rembg.remove(image, session=session) # [H, W, 4]
         mask = carved_image[..., -1] > 0
--- seem/masks.py
+++ seem/masks.py
@@ -1,8 +1,9 @@
-import torch, cv2, os, glob, subprocess, random
+import torch, cv2, os, glob, subprocess, random, yaml
 import numpy as np
 import torch.nn.functional as F
 from PIL import Image
-from torchvision import transforms
+from pytorch_lightning import seed_everything
+from omegaconf import OmegaConf
 from seem.utils.visualizer import Visualizer
 from detectron2.utils.colormap import random_color
 from detectron2.data import MetadataCatalog
@@ -10,6 +11,11 @@ from detectron2.structures import BitMasks
 from .modeling.language.loss import vl_similarity
 from .utils.constants import COCO_PANOPTIC_CLASSES
 from detectron2.data.datasets.builtin_meta import COCO_CATEGORIES
+
+from lama.saicinpainting.evaluation.utils import move_to_device
+from lama.saicinpainting.training.trainers import load_checkpoint
+from lama.saicinpainting.evaluation.data import pad_tensor_to_modulo
+
 
 metadata = MetadataCatalog.get('coco_2017_train_panoptic')
 all_classes = [name.replace('-other','').replace('-merged','') for name in COCO_PANOPTIC_CLASSES] + ["others"]
@@ -21,18 +27,13 @@ from seem.modeling import build_model
 from seem.utils.constants import COCO_PANOPTIC_CLASSES
 from seem.demo.seem.tasks import *
 
-def query_middleware(
-            opt, 
-            image: Image, 
-            reftxt: str, 
-            preloaded_seem_detector = None
-        ):
-    """
-        only query mask&box for single target-noun
-        
-        image: removed pil image
-        reftxt: query target text
-    """
+
+
+
+
+
+
+def preload_seem_detector(opt, preloaded_seem_detector = None):
     if preloaded_seem_detector is None:
         cfg = load_opt_from_config_files([opt.seem_cfg])
         cfg['device'] = opt.device
@@ -44,127 +45,175 @@ def query_middleware(
     with torch.no_grad():
         seem_model.model.sem_seg_head.predictor.lang_encoder.get_text_embeddings(COCO_PANOPTIC_CLASSES + ["background"],
                                                                                  is_eval=True)
-    # get text-image mask
-    width, height = image.size
-    image_ori = np.asarray(image)
-    images = torch.from_numpy(image_ori.copy()).permute(2, 0, 1).cuda()
-    data = {"image": images, "height": height, "width": width}
-    visual = Visualizer(image_ori, metadata=metadata)
-
     seem_model.model.task_switch['spatial'] = False
     seem_model.model.task_switch['visual'] = False
     seem_model.model.task_switch['grounding'] = False
     seem_model.model.task_switch['audio'] = False
     seem_model.model.task_switch['grounding'] = True
 
-    data['text'] = [reftxt]
-    batch_inputs = [data]
+    preloaded_seem_detector = seem_model
+    return preloaded_seem_detector, cfg
 
-    results, image_size, extra = seem_model.model.evaluate_demo(batch_inputs)
-    # print(f'extra.keys() = {extra.keys()}')
-    # print(f'results.keys() = {results.keys()}')
+def preload_lama_remover(opt, preloaded_lama_dict = None):
+    if preloaded_lama_dict is not None: return preloaded_lama_dict
+
+    # seed_everything(opt.seed)
+    predict_config = OmegaConf.load(opt.lama_config)
+    predict_config.model.path = opt.lama_ckpt
+    device = torch.device(opt.device)
+    train_config_path = os.path.join(predict_config.model.path, 'config.yaml')
+
+    with open(train_config_path, 'r') as f:
+        train_config = OmegaConf.create(yaml.safe_load(f))
+
+    train_config.training_model.predict_only = True
+    train_config.visualizer.kind = 'noop'
+
+    checkpoint_path = os.path.join(
+        predict_config.model.path, 'models',
+        predict_config.model.checkpoint
+    )
+    model = load_checkpoint(
+        train_config, checkpoint_path, strict=False, map_location='cpu')
+    model.freeze()
+    if not predict_config.get('refine', False):
+        model.to(device)
+
+    return {
+        'model': model,
+        'config': predict_config
+    }
+
+
+def dilate_mask(mask, dilate_factor=15):
+    if np.max(mask) <= 1.:
+        mask *= 255.
+
+    mask = mask.astype(np.uint8)
+    mask = cv2.dilate(
+        mask,
+        np.ones((dilate_factor, dilate_factor), np.uint8),
+        iterations=1
+    )
+    return mask
+
+
+@torch.no_grad()
+def inpaint_img_with_lama(
+        img: np.ndarray,
+        mask: np.ndarray,
+        mod=8,
+        device="cuda",
+        preloaded_lama_remover=None
+):
+    if isinstance(mask, list):
+        print(f'len(mask) = {len(mask)}')
+        for i in range(len(mask)):
+            print(f'mask[{i}].shape = {mask[i].shape}')
+    else:
+        print(f'mask.shape = {mask.shape}')
+
+    # for mask_ in mask:
+    # assert len(mask_.shape) == 2
+    if len(mask.shape) > 2:
+        mask = mask.squeeze()
+    if np.max(mask) == 1:
+        mask = mask * 255
+    device = torch.device(device)
+
+    img = torch.from_numpy(img).float().div(255.)
+    mask = torch.from_numpy(mask).float()
+    print(' ' * 6 + '-' * 9 + 'loading lama' + '-' * 9)
+
+    model = preloaded_lama_remover['model']
+    predict_config = preloaded_lama_remover['config']
+
+    batch = {}
+    batch['image'] = img.permute(2, 0, 1).unsqueeze(0)
+    batch['mask'] = mask[None, None]
+    unpad_to_size = [batch['image'].shape[2], batch['image'].shape[3]]
+    batch['image'] = pad_tensor_to_modulo(batch['image'], mod)
+    batch['mask'] = pad_tensor_to_modulo(batch['mask'], mod)
+    batch = move_to_device(batch, device)
+    batch['mask'] = (batch['mask'] > 0) * 1
+
+    batch = model(batch)
+    cur_res = batch[predict_config.out_key][0].permute(1, 2, 0)
+    cur_res = cur_res.detach().cpu().numpy()
+
+    if unpad_to_size is not None:
+        orig_height, orig_width = unpad_to_size
+        cur_res = cur_res[:orig_height, :orig_width]
+
+    cur_res = np.clip(cur_res * 255, 0, 255).astype('uint8')
+    return cur_res
+
+
+
+
+def process_seem_outputs(temperature, results, extra):
 
     pred_masks = results['pred_masks'][0]
+
     v_emb = results['pred_captions'][0]
     t_emb = extra['grounding_class']
-
     t_emb = t_emb / (t_emb.norm(dim=-1, keepdim=True) + 1e-7)
     v_emb = v_emb / (v_emb.norm(dim=-1, keepdim=True) + 1e-7)
 
-    temperature = seem_model.model.sem_seg_head.predictor.lang_encoder.logit_scale
-    out_prob = vl_similarity(v_emb, t_emb, temperature=temperature)
-
-    matched_id = out_prob.max(0)[1]
-    pred_masks_pos = pred_masks[matched_id,:,:]
-    pred_masks_pos = (F.interpolate(pred_masks_pos[None,], image_size[-2:], mode='bilinear')[0,:,:data['height'],:data['width']] > 0.0).float().cpu().numpy()
-    # mask queried from text
-    pred_box_pos = None
-    demo = visual.draw_binary_mask(pred_masks_pos.squeeze(), text=reftxt)  # rgb Image
-    res = demo.get_image()
-
-    # TODO: save
-    sam_output_dir = os.path.join(opt.base_dir, 'Semantic')
-    if not os.path.exists(sam_output_dir): os.mkdir(sam_output_dir)
-    name_ = f'./{sam_output_dir}/panoptic'
-    t = 0
-    while os.path.isfile(f'{name_}-{t}.jpg'): t += 1
-    cv2.imwrite(f'{name_}-{t}.jpg', cv2.cvtColor(np.uint8(res), cv2.COLOR_RGB2BGR))
-    print(f'seg result image saved at \'{name_}-{t}.jpg\'')
+    # out_prob = vl_similarity(v_emb, t_emb, temperature=temperature)
+    matched_id = vl_similarity(v_emb, t_emb, temperature=temperature).max(0)[1]
+    pred_masks_pos = pred_masks[matched_id, :, :]
+    # assert isinstance(pred_masks_pos, list) and len(pred_masks_pos) == 1, f'len(pred_masks_pos) = {pred_masks_pos}'
+    return pred_masks, t_emb, v_emb, pred_masks_pos
 
 
-    return Image.fromarray(res), pred_masks_pos, pred_box_pos
+def FG_remove(opt, img, reftxt = 'Cars', preloaded_seem_detector = None, preloaded_lama_dict = None, dilate_kernel_size = 15):
+    # img: np.array -> [H W 3]
+    seem_model, seem_cfg = preload_seem_detector(opt, preloaded_seem_detector)
+    preloaded_lama_dict = preload_lama_remover(opt, preloaded_lama_dict)
+    height, width, _ = img.shape
+    img_ori = np.asarray(img).copy()
+    img = torch.from_numpy(img_ori).permute(2, 0, 1).cuda()
+    visual = Visualizer(img, metadata=metadata)
 
-def middleware(
-        opt, 
-        image: Image, 
-        visual_mode = True,
-        preloaded_seem_detector = None
-    ):
-    """
-        image: target not removed PIL image
-        only to create Panoptic segmentation
-    """
-    if preloaded_seem_detector is  None:
-        cfg = load_opt_from_config_files([opt.seem_cfg])
-        cfg['device'] = opt.device
-        seem_model = BaseModel(cfg, build_model(cfg)).from_pretrained(opt.seem_ckpt).eval().cuda()
-    else:
-        cfg = preloaded_seem_detector['cfg']
-        seem_model = preloaded_seem_detector['seem_model']
-    
-    with torch.no_grad():
-        seem_model.model.sem_seg_head.predictor.lang_encoder.get_text_embeddings(COCO_PANOPTIC_CLASSES + ["background"], is_eval=True)
-    sam_output_dir = os.path.join(opt.base_dir, 'Semantic')
-    res, lists = gain_panoptic_seg(seem_model, image)
-    if np.max(res) <= 1.:
-        res = res * 255.
-    # dif_res, dif_lists = gain_panoptic_seg(seem_model, diffusion_image)
-    cv2.imwrite(f'{sam_output_dir}/panoptic.jpg', cv2.cvtColor(np.uint8(res), cv2.COLOR_BGR2RGB))
-    print(f'panoptic seg saved at \'{sam_output_dir}/panoptic.jpg\'')
-    
-    return res, lists
-
-def gain_panoptic_seg(seem_model, image: Image, visual_mode=True):
-
-    # image_ori = transform(image)
-    # print(image.size)
-    width, height = image.size
-    image_ori = np.asarray(image)
-    images = torch.from_numpy(image_ori.copy()).permute(2, 0, 1).cuda()
-
-    if visual_mode:
-        visual = Visualizer(image_ori, metadata=metadata)
-    data = {"image": images, "height": height, "width": width}
-
-    # inistalize task
-    seem_model.model.task_switch['spatial'] = False
-    seem_model.model.task_switch['visual'] = False
-    seem_model.model.task_switch['grounding'] = False
-    seem_model.model.task_switch['audio'] = False
-    seem_model.model.task_switch['grounding'] = True
-    # seem_model.model.task_switch['bbox'] = True
-    # data['text'] = [reftxt]
+    data = {"image": img, "height": height, "width": width}
+    data['text'] = reftxt # flexible targets
     batch_inputs = [data]
 
-    # if 'Panoptic' in tasks:
-    seem_model.model.metadata = metadata
-    results, mask_box_dict = seem_model.model.evaluate_all(batch_inputs)
-    mask_all, category, masks_list = results[-1]['panoptic_seg']
+    results, image_size, extra = seem_model.model.evaluate_demo(batch_inputs)
+    temperature = seem_model.model.sem_seg_head.predictor.lang_encoder.logit_scale
+    *_, pred_masks_pos = process_seem_outputs(temperature, results, extra)
 
-    # box_list = results[-1]['instances'].pred_boxes
-    # print(f'type(box_list) = {type(box_list)}')
-    # print(f'box_list = {box_list}')
-    # print(f'len(box_list) = {len(box_list)}')
-    # bb = mask_box_dict['boxes'] # key: masks, boxes
-    # print(f'a box: {bb}')
+    pred_masks_pos = (F.interpolate(pred_masks_pos[None,], image_size[-2:], mode='bilinear')[0, :, :data['height'],
+                      :data['width']] > 0.0).float().cpu().numpy() # np.ndarray -> [3 H w]
+    # mask queried from text
+    # pred_box_pos = None
+    demo = visual.draw_binary_mask(pred_masks_pos.squeeze(), text=reftxt)  # rgb Image
+    res = demo.get_image() # visualized with [id2name]
 
-    assert len(category) == len(masks_list), f'len(category) = {len(category)}, len(masks_list) = {len(masks_list)}'
-    object_mask_list = [{
-        'name': metadata.stuff_classes[category[i]['category_id']],
-        'mask': masks_list[i]
-    } for i in range(len(category))]
+    target_mask_list = [dilate_mask(a_mask, dilate_kernel_size) for a_mask in pred_masks_pos]
 
-    demo = visual.draw_panoptic_seg(mask_all.cpu(), category)  # rgb Image
-    res = demo.get_image()
+    # remove forground <CAR>
+    img_inpainted = inpaint_img_with_lama(
+        img = img_ori, mask = target_mask_list[0], mod = 8, device = opt.device, preloaded_lama_remover = preloaded_lama_dict
+    ) # -> np.array([H W 3]) | cv2.imwrite: cv2.cvtColor(np.uint8(img_inpainted), cv2.COLOR_RGB2BGR)
 
-    return Image.fromarray(res), object_mask_list
+    seg_mask = np.concatenate([np.expand_dims(tar, axis=0) for tar in target_mask_list], axis=0)
+    print(f'seg_mask = {seg_mask.shape}')
+
+    # TODO: save intermediate results
+    cv2.imwrite(os.path.join(opt.results, 'remove/r.jpg'), cv2.cvtColor(np.uint8(img_inpainted), cv2.COLOR_RGB2BGR))
+    cv2.imwrite(os.path.join(opt.results, 'remove/m.jpg'), cv2.cvtColor(np.uint8(seg_mask), cv2.COLOR_RGB2BGR))
+
+
+
+    return Image.fromarray(res), seg_mask
+
+
+
+
+
+
+
+
+
